b59171349268e37407bcff8f14abeeb34548c660
worldwidepixel <worldwidepxl@gmail.com>
2025-12-22 22:00:30 -0800

    fix: Whaat?

--- src/lib/server/colours.ts
+++ src/lib/server/colours.ts
@@ -1,3 +1,23 @@
-import type { ReleasePalette } from '$lib/types';
+import { building } from '$app/environment';
+import { fetchReleases, grabToHex } from '$lib';
+import type { Release, ReleasePalette } from '$lib/types';
+import colour from 'colorthief';
+
+const releases = await fetchReleases();
 
 export const releaseColours: { [key: string]: ReleasePalette } = {};
+
+for (const release of releases) {
+	const metadata: Release = release.metadata;
+
+	const palette: ReleasePalette = {
+		slug: metadata.slug,
+		primary: metadata.colour,
+		secondary: grabToHex(await colour.getColor('./static' + metadata.icon)),
+		palette: (await colour.getPalette('./static' + metadata.icon)).map((colour) =>
+			grabToHex(colour)
+		)
+	};
+
+	releaseColours[metadata.slug] = palette;
+}
