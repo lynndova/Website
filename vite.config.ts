9114c553b6aac7cb9f2760ae484cdb98d7fbff55
worldwidepixel <worldwidepxl@gmail.com>
2026-01-04 09:38:29 -0800

    tweak: Random bullshit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), cloudflare()],
+	ssr: {
+		external: ['node:fs', 'node:path', 'node:util', 'node:stream', 'node:zlib', 'node:assert']
+	}
 });
