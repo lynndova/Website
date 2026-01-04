5f6bfb885bb9e34a290432980be4f4d4aa83e171
worldwidepixel <worldwidepxl@gmail.com>
2026-01-04 09:49:21 -0800

    feat: throwing shit at the wall

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { cloudflare } from '@cloudflare/vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	ssr: {
-		external: ['node:fs', 'node:path', 'node:util', 'node:stream', 'node:zlib', 'node:assert']
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
