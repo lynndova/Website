6c9664e78519a7e2d8d6de661c338826f2c70c81
worldwidepixel <worldwidepxl@gmail.com>
2026-02-16 08:43:38 -0800

    tweak: We're trying anything and everything again

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	resolve: {
+		alias: {
+			fs: 'node:fs',
+			path: 'node:path',
+			util: 'node:util',
+			stream: 'node:stream',
+			zlib: 'node:zlib',
+			assert: 'node:assert'
+		}
+	}
 });
